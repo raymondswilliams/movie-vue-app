b458043b8b6d3380ffe33c7f7382eee5361b51b6
shane <willyums86@gmail.com>
2021-10-01 11:50:18 -0400

    Adds new movie-vue-app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <div v-if="newMovie">
+      Title: {{ newMovie.title }} Year: {{ newMovie.year }} Director:
+      {{ newMovie.director }} Plot: {{ newMovie.plot }}
+    </div>
+    <div>Title: <input type="text" v-model="newMovieParams.title" /></div>
+    <div>Year: <input type="text" v-model="newMovieParams.year" /></div>
+    <div>Plot: <input type="text" v-model="newMovieParams.plot" /></div>
+    <div>Director: <input type="text" v-model="newMovieParams.director" /></div>
+    <button v-on:click="createMovie()">Create Movie</button>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <p>{{ movie.title }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to The Movies!",
+      movies: [],
+      newMovieParams: {},
+      newMovie: null,
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      var params = this.newMovieParams;
+      axios.post("http://localhost:3000/movies", params).then((response) => {
+        this.movies.push(response.data);
+        this.newMovieParams.title = "";
+        this.newMovieParams.year = "";
+        this.newMovieParams.plot = "";
+        this.newMovieParams.director = "";
+      });
+    },
+    showMovie: function () {
+      axios
+        .get("http://localhost:3000/movies/" + this.movieId)
+        .then((response) => {
+          this.newMovie = response.data;
+        });
+    },
   },
 };
 </script>
